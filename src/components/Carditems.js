83f39e23bd6c72eeda2629a6abcc83ae48eb43e1
Aliya Ijaz <aliyaijaz786@gmail.com>
2022-01-31 11:43:43 +0500

    updated

--- src/components/Carditems.js
+++ src/components/Carditems.js
@@ -3,23 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 function Carditems(props) {
-    
-    //const cardMove = useWebAnimations({...bounceInLeft});
-    
+
+
     return (
         <>
-            {/* <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft'> */}
-           <li className="cards__item">
-               <Link className="cards__item__link" to={props.path}>
-                  <figure className="cards__item__pic-wrap" data-category={props.label}>
-                     <img src={props.src} alt="Travel " className="cards__item__img" /> 
-                  </figure> 
-                  <div className="cards__item__info">
-                      <h5 className="cards__item__text">{props.text}</h5>      
-                  </div>
-               </Link>
-            </li> 
-            {/* </ScrollAnimation> */}
+            <li className="cards__item">
+                <Link className="cards__item__link" to={props.path}>
+                    <figure className="cards__item__pic-wrap" data-category={props.label}>
+                        <img src={props.src} alt="Travel " className="cards__item__img" />
+                    </figure>
+                    <div className="cards__item__info">
+                        <h5 className="cards__item__text">{props.text}</h5>
+                    </div>
+                </Link>
+            </li>
         </>
     );
 }
